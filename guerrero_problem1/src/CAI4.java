c1c8c374fefea1798753a528805fec1505e1e649
jaime Guerrero <jaimegordon20@outlook.com>
2020-02-20 17:33:12 -0500

    third  commit

--- guerrero_problem1/src/CAI4.java
+++ guerrero_problem1/src/CAI4.java
@@ -8,37 +8,7 @@ import java.util.Scanner;
 //At a difficulty level of 1, the program should use only single-digit numbers 
 //in the problems; at a difficulty level of 2, numbers as large as two digits, and so on.
 //Allow for four levels of difficulty.
-//
-//
-//
-//Requirements
-//The program shall generate random numbers with a SecureRandom object
-//The program shall ask the student to enter a difficulty level of 1, 2, 3, or 4
-//A difficulty level of 1 shall limit random numbers to the range of 0-9, inclusive
-//A difficulty level of 2 shall limit random numbers to the range of 0-99, inclusive
-//A difficulty level of 3 shall limit random numbers to the range of 0-999, inclusive
-//A difficulty level of 4 shall limit random numbers to the range of 0-9999, inclusive
-//The program shall ask the student to solve 10 different multiplication problems
-//Multiplication problems shall contain two numbers sampled from a uniform random distribution with bounds determined by the problem difficulty
-//The program shall display a random positive message if the student provides a correct response
-//The program shall display a random negative message if the student provides an incorrect response
-//The program shall display the student's score after the student has attempted to solve 10 problems
-//The student's score shall be the percentage of problems correctly solved
-//The program shall display the message "Please ask your teacher for extra help." if the student's score is less than 75%
-//The program shall display the message "Congratulations, you are ready to go to the next level!" if the student's score is greater than or equal to 75%
-//The program shall ask the student if they want to solve a new problem set after the score message has been displayed
-//The program shall restart when the student agrees to solve a new problem set
-//The program shall terminate when the student declines to solve another problem set
-//Create a method called "quiz" that contains the program logic
-//Create a function called "readDifficulty" that reads the difficulty level from the student
-//Create a function called "generateQuestionArgument" that uses the difficulty level to generate a random number
-//Create a function called "askQuestion" that prints the problem to the screen
-//Create a function called "readResponse" that reads the answer from the student
-//Create a function called "isAsnwerCorrect" that checks to see if the student's answer matches the correct answer to the problem
-//Create a function called "displayCorrectResponse" that prints out the response when a student enters a correct answer
-//Create a function called "displayInorrectResponse" that prints out the response when a student enters an incorrect answer
-//Create a function called "displayCompletionMessage" that prints out the studen't score and appropriate score response
-//Create a main method that runs your program by calling the "quiz" method
+
  
 public class CAI4 {
 	int total;
@@ -66,22 +36,10 @@ public class CAI4 {
 
 	public void quiz() 
 	{
-		//CAI3 total = new CAI3();
-		
-	    
-		// loop for 10 question
 		readDifficulty();
-		//for (int i =0; i<10; i++)
-		//{
-		generateQuestionArgument();	
+        generateQuestionArgument();	
 		askQuestion();
-		//readResponse();
-		//isAsnwerCorrect()
-		;
-	   // }
-		displayCompletionMessage();
-		
-		
+	    displayCompletionMessage();
 	}
 	
 	public void readDifficulty()
@@ -94,21 +52,16 @@ public class CAI4 {
 	    System.out.println('\t'+"4)level four");   
 	    this.selectLevel = input.nextInt();
 	    input.nextLine();
-		//return selectLevel;
+		
 	}
 	
 	public void generateQuestionArgument()
 	{
-//		int  compare1 =0;
-//	    int  compare2 =0;
-		switch(this.selectLevel)
+        switch(this.selectLevel)
 		{
 		case 1:
-			
-				
 			this.number1    =   1   +   rand.nextInt(9); 
 			this.number2   =    1   +   rand.nextInt(9);
-			
 			break;
 		
 		case 2:
@@ -162,32 +115,11 @@ public class CAI4 {
 	    		}
 			
 	    	}
-			//if statement avoid generated equal ramdon number
-//		if ( this.number1==this.number2)
-//		{
-//		   // this.number1    =    1    +    ramdon1.nextInt(9); 
-//		    //this.number2   =    1    +    ramdon1.nextInt(9);
-//		    System.out.printf("how much is %d ",    number1);
-//			System.out.printf("time %d        ", number2   );    
-//			System.out.println(  ) ;
-//	    }
-//		/// Print random number
-//		else 
-//	    {
-////		    display    generated    value
-//		    System.out.printf("how much is %d ",    number1);
-//		    System.out.printf("time %d        ", number2   );    
-//		    System.out.println(  ) ;
-//		}
-		
-		
- 
 	}
 	
 
 	public int readResponse()
 	{
-		
 		System.out.println("please enter your answer ");
 		this.userInput = input.nextInt();
 		input.nextLine();
@@ -213,9 +145,8 @@ public class CAI4 {
 	public void displayCorrectResponse() 
 	{
 		correctAnwers++; // counter how many good answers
-		//this.number1    =    1    +    ramdon1.nextInt(9); 
 		int rand_int1 = 1+rand.nextInt(4); 
-		//System.out.println(rand_int1);
+		
 		switch(rand_int1)
 		{
 		case 1:
@@ -232,19 +163,12 @@ public class CAI4 {
 			break;
 			
 		}
-		
-		
-		
-		
 	}
 	
 	public void displaynoCorrectResponse() 
 	{	
 		
 		wrongAnwers++; // counter how many bad answers
-		
-		//System.out.println("No, please try again");
-		
 		int rand_int2 = 1+rand.nextInt(4); 
 		switch(rand_int2)
 		{
@@ -263,10 +187,8 @@ public class CAI4 {
 			
 		}
 		
-		
-		
-		
 	}
+	
 	public void displayCompletionMessage()
 	{
 		//percentage calculation
